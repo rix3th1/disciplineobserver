26581707ca9a530cbc2cc67865c5cf6a4973de94
Ricardo Rojas <rojasricor@gmail.com>
2023-06-04 21:30:30 -0500

    ok

--- app/database/db.sql
+++ app/database/db.sql
@@ -10,6 +10,9 @@ CREATE TABLE `citations` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+INSERT INTO `citations` (`_id`, `citation_date`, `msg_parent`, `email_parent`, `created_at`) VALUES
+(1111122448, '2023-06-04 22:23:00', 'Estudiante paso las pruebas saber con execelencia', 'rojasricor@gmail.com', '2023-06-04 21:23:55');
+
 CREATE TABLE `grades` (
   `_id` varchar(10) NOT NULL,
   `grade` varchar(10) NOT NULL
@@ -37,6 +40,9 @@ CREATE TABLE `notations` (
   `testimony` text NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+INSERT INTO `notations` (`_id`, `notation`, `grade`, `testimony`, `created_at`) VALUES
+(1111122448, 'Celebración por qué el estudiante se destacó ', '11th', 'Hajshs', '2023-06-04 21:23:55');
 
 CREATE TABLE `roles` (
   `_id` enum('teacher','parent','rector','secretary') NOT NULL,
@@ -72,7 +78,8 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 INSERT INTO `users` (`_id`, `name`, `lastname`, `telephone`, `email`, `password`, `role`) VALUES
-(65701167, 'Amaya Head', 'Drake', '3173926578', 'rrojas48@itfip.edu.co', '$2y$10$QcJBAwGOrTlmamfDIyybbu7IRHqKDwGAOvKkdnIL4d0p6DMdgEM0G', 'parent'),
+(65701167, 'Amaya Head', 'Drake', '3173926578', 'rrojas48@itfip.edu.co', '$2y$10$QcJBAwGOrTlmamfDIyybbu7IRHqKDwGAOvKkdnIL4d0p6DMdgEM0G', 'secretary'),
+(1005772256, 'Andrés', 'Mayorquin', '3203048789', 'andrewpark2345@gmail.com', '$2y$10$.YZB6Zj5mhy/2gEpdcZSIuR4ecS7dhRX0TnDkr8zDB8.mpCqlWkpu', 'teacher'),
 (1111122448, 'Ricardo Andrés', 'Rojas Rico', '3173926578', 'rojasricor@gmail.com', '$2y$10$45.eYqwqNk8fBENaE.c/VuiFs31jikvTEm2aTRcJYtFc92yBPlcH2', 'rector');
 
 
@@ -99,7 +106,7 @@ ALTER TABLE `users`
 
 
 ALTER TABLE `notations`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1111122449;
 
 ALTER TABLE `students`
   MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1111122451;
